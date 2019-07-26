925e84fb4fdd02fcd77b79e83806e9d375dd569d
Manoliya <51356264+Manoliya@users.noreply.github.com>
2019-07-26 19:30:00 +0530

    Update ex18.py

--- pro1/ex18.py
+++ pro1/ex18.py
@@ -1,16 +1,16 @@
 n7,m7=map(int,input().split())
-te=[]
+tem=[]
 x7=0
 for m in range(n7):
-    te.append(list(map(int,input().split())))   
+    tem.append(list(map(int,input().split())))   
 for m7 in range(n7):
     for j in range(m7-1):
         for k in range(j+1,m7+1):
-            if te[m7][j:k]==[1]*len(te[m7][j:k]):
-                 if all(te[p+m7][j:k]==[1]*len(te[p+m7][j:k]) for p in range(len(te[m7][j:k])-1)):
-                     if len(te[m7][j:k])>x7:
-                        x7=len(te[m7][j:k])
-if len(te)==1 and len(te[0])==1 and te[0][0]==1:
+            if tem[m7][j:k]==[1]*len(tem[m7][j:k]):
+                 if all(tem[p+m7][j:k]==[1]*len(tem[p+m7][j:k]) for p in range(len(tem[m7][j:k])-1)):
+                     if len(tem[m7][j:k])>x7:
+                        x7=len(tem[m7][j:k])
+if len(tem)==1 and len(tem[0])==1 and tem[0][0]==1:
     print(1)
 for m7 in range(x7):
     print(*[1]*x7) 
